5975d84bda1fdeffde6dd17f1f529e83c665cda5
natchapon022 <natchapon.wak@student.mahidol.edu>
2025-11-13 05:07:44 +0000

    added test cases

--- test_calculator.py
+++ test_calculator.py
@@ -11,5 +11,25 @@ class TestCalculator(unittest.TestCase):
 
     # Add the following test methods to the TestCalculator class:
 
+    def test_add1(self):
+        self.assertEqual(self.calc.add(2, 2), 4)
+        self.assertEqual(self.calc.add(-1, 2), 1)
+        
+    def test_subtract1(self):
+        self.assertEqual(self.calc.subtract(1, 2), -1)
+        self.assertEqual(self.calc.subtract(-2, 1), -3)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 2), 8)
+        self.assertEqual(self.calc.multiply(2, 0), 0)
+
+    def test_divide1(self):
+        self.assertEqual(self.calc.divide(4, 2), 2)
+        self.assertEqual(self.calc.divide(6, 2), 3)
+
+    def test_modulo1(self):
+        self.assertEqual(self.calc.modulo(7, 2), 1)
+        self.assertEqual(self.calc.modulo(9, 0), 9)
+
 if __name__ == '__main__':
     unittest.main()
